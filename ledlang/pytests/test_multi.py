711bd975ddf2841bad55087d0d5c5d3fe16ba51d
ElliNet13 <elliotmoose4@gmail.com>
2025-07-22 16:17:00 -0600

    Add tests, LINE IS BROKEN

--- ledlang/pytests/test_multi.py
+++ ledlang/pytests/test_multi.py
@@ -4,7 +4,7 @@ import serial
 import threading
 from ledlang import MultiLEDLang, PytestLEDDeviceSimulator
 
-def test_multi():
+def test_multisides():
     simulator1 = PytestLEDDeviceSimulator("3x3")
     simulator2 = PytestLEDDeviceSimulator("3x3")
     threading.Thread(target=simulator1.run, daemon=True).start()
@@ -40,4 +40,42 @@ def test_multi():
         [0, 1, 1],
         [0, 1, 1],
         [0, 1, 1],
+    ]
+
+def test_multichess():
+    simulator1 = PytestLEDDeviceSimulator("3x3")
+    simulator2 = PytestLEDDeviceSimulator("3x3")
+    threading.Thread(target=simulator1.run, daemon=True).start()
+    threading.Thread(target=simulator2.run, daemon=True).start()
+    displays = {
+        'size': '3x3',
+        'displays': [
+            {
+                'serial': simulator1.serial,
+                'rotation': 0
+            },
+            {
+                'serial': simulator2.serial,
+                'rotation': 0
+            },
+        ]
+    }
+    multi = MultiLEDLang(displays)
+    multi.play(multi.compile("""
+    INIT 6x3
+    DISPLAY [
+    0 1 0 1 0 1
+    1 0 1 0 1 0
+    0 1 0 1 0 1
+    ]
+    """))
+    assert simulator1.kill() == [
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0],
+    ]
+    assert simulator2.kill() == [
+        [1, 0, 1],
+        [0, 1, 0],
+        [1, 0, 1],
     ]
